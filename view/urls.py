393611b9b7c41ef5ebbdc37e222381d1fd5be1cf
AhamedMuaaz <mma.muaaz@gmail.com>
2019-04-20 19:23:06 +0530

    added route within app

--- view/urls.py
+++ view/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path,include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('',views.home,name='view-home'),
+     path('about/',views.about,name='view-about'),
 ]
--- view/views.py
+++ view/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
+    m=2*2
     return HttpResponse('<h1>View Home</h1>')
+
+def about(request):
+    return HttpResponse('<h1>View about</h1>')
