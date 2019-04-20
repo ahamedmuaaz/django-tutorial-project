adbcc9105fe5b80e9f297bd25dc44107c5fe9773
AhamedMuaaz <mma.muaaz@gmail.com>
2019-04-20 21:05:08 +0530

    temporary data store

--- view/views.py
+++ view/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts=[
+    {
+       'Name':'Muaaz',
+       'Age':'22',
+       'Job':'Software Engineer'
+    },
+    {
+       'Name':'Roy',
+       'Age':'24',
+       'Job':'QA'
+
+    }
+]
 # Create your views here.
 def home(request):
+    context={
+        'posts':posts
+    }
     m=2*2
-    return render(request,'view/home.html')
+    return render(request,'view/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>View about</h1>')
+    return render(request,'view/about.html')
